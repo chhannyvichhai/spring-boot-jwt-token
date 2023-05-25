c46558c001202ff19db59d1e34b1cafdeac1c93c
Chhanny-Vichhai <vichhai212803@gmail.com>
2023-05-25 15:18:40 +0700

    code jwt token

---
+++ src/main/java/com/chhai/friendlyjwt/FriendlyJwtApplication.java
@@ -0,0 +1,13 @@
+package com.chhai.friendlyjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendlyJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendlyJwtApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/chhai/friendlyjwt/controller/AuthenticationController.java
@@ -0,0 +1,19 @@
+package com.chhai.friendlyjwt.controller;
+
+import com.chhai.friendlyjwt.service.TokenService;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    TokenService tokenService;
+    AuthenticationController (TokenService tokenService){
+        this.tokenService = tokenService;
+    }
+    @PostMapping("/token")
+    public String getToken(Authentication authentication){
+        String token = tokenService.generateToken(authentication);
+        return token;
+    }
+}
