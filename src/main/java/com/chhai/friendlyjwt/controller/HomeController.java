64f0d5b96bdd30a1581ae305504d737d6a93e86c
Chhanny-Vichhai <vichhai212803@gmail.com>
2023-05-25 15:44:04 +0700

    update code homeController use logger

--- src/main/java/com/chhai/friendlyjwt/controller/HomeController.java
+++ src/main/java/com/chhai/friendlyjwt/controller/HomeController.java
@@ -1,13 +1,25 @@
 package com.chhai.friendlyjwt.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HomeController {
 
+
+@RestController
+@Slf4j
+public class HomeController {
+    Logger logger =  LoggerFactory.getLogger(HomeController.class);
     @GetMapping("/home")
-    public String homepage(){
-        return("Hello ! This is home page");
+    public String homepage(Authentication authentication){
+        var user = authentication.getPrincipal();
+        logger.info("User is : {}",authentication.getPrincipal());
+        logger.info("User is : {}", authentication.getCredentials());
+        logger.info("User is : {}", authentication.getDetails());
+        logger.info("User is : {}", authentication.getAuthorities());
+        return("Hello ! " + authentication.getName());
     }
 }
